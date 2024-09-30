a52cac96226bb3fb2480d7c8573640ba6a4b89e4
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-10-01 03:21:02 +0900

    [section4] appconfig 리팩토링 - 역할과 구현 한눈에 보이게

--- spring/src/main/java/spring_study/spring/spring_core_basic/AppConfig.java
+++ spring/src/main/java/spring_study/spring/spring_core_basic/AppConfig.java
@@ -1,8 +1,10 @@
 package spring_study.spring.spring_core_basic;
 
+import spring_study.spring.spring_core_basic.Discount.DiscountPolicy;
 import spring_study.spring.spring_core_basic.Discount.FixDiscountPolicy;
 import spring_study.spring.spring_core_basic.Order.OrderService;
 import spring_study.spring.spring_core_basic.Order.OrderServiceImpl;
+import spring_study.spring.spring_core_basic.member.MemberRespository;
 import spring_study.spring.spring_core_basic.member.MemberService;
 import spring_study.spring.spring_core_basic.member.MemberServiceImpl;
 import spring_study.spring.spring_core_basic.member.MemoryMemberRespository;
@@ -17,15 +19,24 @@ public class AppConfig {
     // 이것이 생성자 주입. .
     public MemberService memberService(){
         return new MemberServiceImpl
-                (new MemoryMemberRespository());
+                (memberRespository());
+    }
+
+    // 리턴타입이 역할, 리턴하는게 구현클래스
+    private MemberRespository memberRespository() {
+        return new MemoryMemberRespository();
     }
 
     // 여기서는 쓸 구현체를 명시해서 넣어주고 원래의 orderservicimple에서는
     // 부모의(?) 타입으로 해서 넘겨주기, 인터페이스에만 의존할 수 있게
     public OrderService orderService(){
         return new OrderServiceImpl(
-                new MemoryMemberRespository(),
-                new FixDiscountPolicy()
+                memberRespository(),
+                discountPolicy()
         );
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
